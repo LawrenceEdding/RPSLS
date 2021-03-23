a8507a471f749eea631631867307d571864316e9
Lee X <lee52597@gmail.com>
2021-03-23 13:16:14 -0400

    Added functionality to determining outcome of game.

--- classes.py
+++ classes.py
@@ -7,6 +7,15 @@ class Character:
 
     def showdown(self, opponent):
         outcome = self.chosen_move.check_match(self.chosen_move, opponent.chosen_move)
+        if outcome == 'win':
+            self.points += 1
+            self.chosen_move.win_term(opponent.chosen_move.name)
+        elif outcome == 'loss':
+            opponent.points += 1
+            opponent.chosen_move.win_term(self.chosen_move.name)
+
+        elif outcome == 'tie':
+            print(f'DRAW!')
         # run function for gesture
 
 
@@ -25,11 +34,17 @@ class Gesture:
         self.name = ''
         self.wins_vs = []
         self.loses_vs = []
+        self.win_text = []
 
-    def assign_gesture(self, name, wins, loses):
+    def assign_gesture(self, name, wins, loses, text):
         self.name = name
         self.wins_vs = wins
         self.loses_vs = loses
+        self.win_text = text
+
+    def win_term(self, loser_name):
+        victory_index = self.loses_vs.index(loser_name)
+        print(f'{self.name} {self.win_text[victory_index]} {loser_name}')
 
     def check_match(self, opposing_gesture):
         if opposing_gesture.name in self.wins_vs:
@@ -50,28 +65,28 @@ class Game:
 class Rock(Gesture):
     def __init__(self):
         super().__init__()
-        super().assign_gesture('rock', ['scissors', 'lizard'], ['paper', 'spock'])
+        super().assign_gesture('rock', ['scissors', 'lizard'], ['paper', 'spock'], ['crushes', 'crushes'])
 
 
 class Paper(Gesture):
     def __init__(self):
         super().__init__()
-        super().assign_gesture('paper', ['rock', 'spock'], ['scissors', 'lizard'])
+        super().assign_gesture('paper', ['rock', 'spock'], ['scissors', 'lizard'], ['covers', 'disproves'])
 
 
 class Scissors(Gesture):
     def __init__(self):
         super().__init__()
-        super().assign_gesture('scissors', ['paper', 'lizard'], ['rock', 'spock'])
+        super().assign_gesture('scissors', ['paper', 'lizard'], ['rock', 'spock'], ['cuts', 'decapitates'])
 
 
 class Lizard(Gesture):
     def __init__(self):
         super().__init__()
-        super().assign_gesture('lizard', ['spock', 'paper'], ['rock', 'scissors'])
+        super().assign_gesture('lizard', ['spock', 'paper'], ['rock', 'scissors'], ['poisons', 'eats'])
 
 
 class Spock(Gesture):
     def __init__(self):
         super().__init__()
-        super().assign_gesture('spock', ['scissors', 'rock'], ['paper', 'lizard'])
+        super().assign_gesture('spock', ['scissors', 'rock'], ['paper', 'lizard'], ['smashes', 'vaporizes'])
