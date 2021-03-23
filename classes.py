94b04dca592c684a08558d8137d65696ad727df3
Lee X <lee52597@gmail.com>
2021-03-23 10:41:29 -0400

    initialized variables in classes. Assigned each gesture a matchup spread. created an outcome method NONFUNCTIONAL.

--- classes.py
+++ classes.py
@@ -2,10 +2,11 @@
 
 class Character:
     def __init__(self):
-        points = 0
-        chosen_move = Gesture()
+        self.points = 0
+        self.chosen_move = Gesture()
 
     def showdown(self, opponent):
+        outcome = self.chosen_move.check_match(self.chosen_move, opponent.chosen_move)
         # run function for gesture
 
 
@@ -21,9 +22,14 @@ class Cpu(Character):
 
 class Gesture:
     def __init__(self):
-        name = ''
-        wins_vs = []
-        loses_vs = []
+        self.name = ''
+        self.wins_vs = []
+        self.loses_vs = []
+
+    def assign_gesture(self, name, wins, loses):
+        self.name = name
+        self.wins_vs = wins
+        self.loses_vs = loses
 
     def check_match(self, opposing_gesture):
         if opposing_gesture.name in self.wins_vs:
@@ -36,32 +42,36 @@ class Gesture:
 
 class Game:
     def __init__(self):
-        p1 = Character()
-        p2 = Character()
-        match = 0
+        self.p1 = Character()
+        self.p2 = Character()
+        self.match = 0
 
 
 class Rock(Gesture):
     def __init__(self):
         super().__init__()
-        win_vs = ['']
+        super().assign_gesture('rock', ['scissors', 'lizard'], ['paper', 'spock'])
 
 
 class Paper(Gesture):
     def __init__(self):
         super().__init__()
+        super().assign_gesture('paper', ['rock', 'spock'], ['scissors', 'lizard'])
 
 
 class Scissors(Gesture):
     def __init__(self):
         super().__init__()
+        super().assign_gesture('scissors', ['paper', 'lizard'], ['rock', 'spock'])
 
 
 class Lizard(Gesture):
     def __init__(self):
         super().__init__()
+        super().assign_gesture('lizard', ['spock', 'paper'], ['rock', 'scissors'])
 
 
 class Spock(Gesture):
     def __init__(self):
         super().__init__()
+        super().assign_gesture('spock', ['scissors', 'rock'], ['paper', 'lizard'])
